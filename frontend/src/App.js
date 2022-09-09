c17e80e68814e06bde52378cf62b60446a216a36
Mark <vargamarklevente@gmail.com>
2022-09-09 23:44:06 +0200

    added product cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,27 @@ function App() {
       <header>
         <a href="/">kamuzon</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((prod) => (
+            <div className="product" key={prod.slug}>
+              <a href={`/product/${prod.slug}`}>
+                <img src={prod.image} alt={prod.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${prod.slug}`}>
+                  <p>{prod.name}</p>
+                </a>
+                <p>
+                  <strong>${prod.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
